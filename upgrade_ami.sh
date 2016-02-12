6428c7c67edddb74a8d1bed0a7ce5520de223785
John Pellman <pellman.john@gmail.com>
2016-02-12 10:41:08 -0500

    Update upgrade_ami.sh

--- upgrade_ami.sh
+++ upgrade_ami.sh
@@ -4,6 +4,7 @@ cd /tmp
 wget https://raw.githubusercontent.com/FCP-INDI/CPAC-config/master/Provisioning/cpac_provision.sh
 chmod +x cpac_provision.sh
 /tmp/cpac_provision.sh -p
+source /etc/bash.bashrc
 /tmp/cpac_provision.sh -n "cpac"
 rm /tmp/cpac_provision.sh
-/opt/afni/@update.afni.binaries
+/opt/afni/@update.afni.binaries -bindir /opt/afni
