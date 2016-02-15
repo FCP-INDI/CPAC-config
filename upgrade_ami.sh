6f1bf5f6862a087bc5b10e22c2ff2790f1f17f22
John Pellman <pellman.john@gmail.com>
2016-02-15 15:02:05 -0500

    Update upgrade_ami.sh

--- upgrade_ami.sh
+++ upgrade_ami.sh
@@ -4,6 +4,7 @@ provision=/root/CPAC-config/Provisioning/cpac_provision.sh
 chmod +x ${provision}
 ${provision} -p
 source /etc/bash.bashrc
+source activate cpac
 ${provision} -n "cpac"
 /opt/afni/@update.afni.binaries -bindir /opt/afni
 mv /root/bashrc /root/.bashrc
