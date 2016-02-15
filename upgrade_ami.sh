0b5511adf06a2747c6102352c5d3246ce7b2037a
John Pellman <pellman.john@gmail.com>
2016-02-15 14:05:42 -0500

    Changes to upgrade_ami.sh for the base AMI.

--- upgrade_ami.sh
+++ upgrade_ami.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd /tmp
-wget https://raw.githubusercontent.com/FCP-INDI/CPAC-config/master/Provisioning/cpac_provision.sh
-chmod +x cpac_provision.sh
-/tmp/cpac_provision.sh -p
+provision=/root/CPAC-config/Provisioning/cpac_provision.sh
+chmod +x ${provision}
+${provision} -p
 source /etc/bash.bashrc
-/tmp/cpac_provision.sh -n "cpac"
-rm /tmp/cpac_provision.sh
+${provision} -n "cpac"
 /opt/afni/@update.afni.binaries -bindir /opt/afni
+mv /root/bashrc /root/.bashrc
